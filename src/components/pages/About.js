0676ebc4a0ed4abb24d5e5c38b92f354b7e46fac
Aaron Thomas <thomasattraction@yahoo.com>
2024-04-06 15:45:01 +0100

    updated

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,10 +8,13 @@ const About = () => {
     <header>About Us</header>
  
    <body> 
-    <HappyCouple HappyCoupleImage={HappyCoupleImage} height={540} width={540}/> 
+    <HappyCouple HappyCoupleImage={HappyCoupleImage} height={510} width={510}/> 
 
     <p className = "description">
-        
+{/*Placeholder */}
+<h3>How we met</h3>
+Enamel pin brunch subway tile twee bicycle rights disrupt migas venmo cray banjo offal pressed. Copper vegan kale chips tousled chambray. The mixtape tofu pabst tilde whatever coloring book.
+Sustainable gentrify kitsch ethical stumptown, plaid fashion cold pressed wayfarers chia artisan. Intelligentsia chambray tousled, kitsch Godard actually pop-up. Listicle ugh flannel tousled roof party. Tofu ethical lumbersexual distillery freegan cardigan authentic keffiyeh.
     </p>
     </body> 
     
