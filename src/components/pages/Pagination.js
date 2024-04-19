566c062a48add1060586286a14ff56778c56d221
Aaron Thomas <thomasattraction@yahoo.com>
2024-04-19 22:48:43 +0100

    pagination updated

--- src/components/pages/Pagination.js
+++ src/components/pages/Pagination.js
@@ -2,21 +2,20 @@ import React, {useState} from 'react';
 
 
 const Pagination = () => { 
-  const currentPage = 1;   
-const paginationData = [{currentPage, setStatePage}] = useState(0); 
+    
+const [currentPage, setCurrentPage] = useState(1); 
 
 
 const nextButton = (e) => {
     e.preventDefault();
-   setStatePage(currentPage + 1);
+   setCurrentPage(currentPage + 1);
     }
 
     const prevButton = (e) => {
         e.preventDefault();
-        setStatePage(currentPage - 1)
+        setCurrentPage(currentPage - 1)
     }
 
-};
 
     return (
               <div className = "container"> 
@@ -24,28 +23,32 @@ const nextButton = (e) => {
             {currentPage === 1 && (  
     <div className = "questionOne">
 <h3>Please enter your name:</h3>
+
         <input name = "firstname" placeholder = "first name" id="firstName"></input>
         <input name = "lastname" placeholder = "last name" id="lastName"></input>
-        <button className = "nextButton" onClick = {nextButton}>Next</button>
+        <button type = "button" className = "nextButton" onClick = {nextButton}>Next</button>
     </div>
-)};
+)}
 
 {currentPage === 2 && ( 
   <div className = "questionTwo">
     <h3>Will you be attending the wedding?</h3>
     <p> 
-    <input type = "radio" id="attending">Attending</input>
-    <input type = "radio" id = "notAttending">Not Attending</input>
-    <button type = "submit" className = "previousButton" onClick = {prevButton}>Previous</button>
-    <button type = "submit" className = "nextButton" onClick = {nextButton}>Next</button>  
+    <input type = "radio" id="attending" value = "Attending"></input>   
+    <label htmlFor = "attending">Attending</label> 
+    
+    
+    <input type = "radio" id = "notAttending" value = "Not Attending">Not Attending/</input>
+    <label htmlFor = "notAttending">Not Attending</label>
+    <button type = "button" className = "previousButton" onClick = {prevButton}>Previous</button>
+    <button type = "button" className = "nextButton" onClick = {nextButton}>Next</button>  
     </p>
 </div>  
-)};
-
+)}
  </form>     
    </div>
     )
-
+};
 
 
 export default Pagination;
