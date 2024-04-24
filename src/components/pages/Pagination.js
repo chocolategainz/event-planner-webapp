3fc0fa0152189dd3750dce555cbaf07a636b174f
Aaron Thomas <thomasattraction@yahoo.com>
2024-04-24 23:27:46 +0100

    pagination corrections for required attribute

--- src/components/pages/Pagination.js
+++ src/components/pages/Pagination.js
@@ -1,16 +1,41 @@
 import React, {useState} from 'react';
-
-
 const Pagination = () => { 
-    
-const [currentPage, setCurrentPage] = useState(1); 
-const [requiredValue , setRequiredValue] = useState("");
 
+  const [inputFields , setInputFields] = useState({
+    name: "" ,
+    surname: "" ,
+    email: "" 
+   }); 
+
+const [error , setError] = useState({});
+const [submission , setSubmission] = useState(false);
+  
+const validateData = (inputFields) => {
+  let errors = {};
+  if (inputFields.name.length < 3) {
+    errors.name = "Name is invalid";
+  }
+  if(inputFields.surname.length < 4) {
+errors.surname = "Surname is invalid";
+  }
+  if (inputFields.email.length < 15) {
+    errors.email = "Email is invalid";
+  }
+  return errors; 
+};
+console.log(validateData); 
+
+const handleStateChange = (e) => {
+  setInputFields({... inputFields, [e.target.name]: e.target.value});
+  setInputFields({... inputFields, [e.target.surname]: e.target.value});
+  setInputFields({... inputFields, [e.target.email]: e.target.value});
+};
+
+const [currentPage, setCurrentPage] = useState(1); 
 
 const nextButton = (e) => {
     e.preventDefault();
    setCurrentPage(currentPage + 1);
-
    if (currentPage === 2) {
      const attending = document.getElementById("attending").checked;
       setCurrentPage(attending ? 3 : 4);
@@ -18,12 +43,10 @@ const nextButton = (e) => {
       setCurrentPage(currentPage + 1);
     }    
   };
-
     const prevButton = (e) => {
         e.preventDefault();
         setCurrentPage(currentPage - 1)
     }
-
     
     const submit = (e) => {
     e.preventDefault();
@@ -31,13 +54,10 @@ const nextButton = (e) => {
 alert("Thank you for submitting a message to us.");
 console.log(note);
     }
-
     const finished = (e) => {
        e.preventDefault();
        alert("Thank you! You are now officially added to our guestbook. All of your details will be forwarded to ...")
     }
-
-
 const reset = () => {
   document.getElementById("resetButton");
 setCurrentPage(1);
@@ -46,20 +66,38 @@ setCurrentPage(1);
 
     return (
               <div className = "container"> 
-              <form onClick={(e) => e.preventDefault()}> 
-
+              <form onSubmit = {submission}> 
             {currentPage === 1 && (  
     <div className = "questionOne">
 <h3>Please enter your name:</h3>
 
-        <input name = "firstname" placeholder = "first name" id="firstName" value = {requiredValue} onChange = {(e) => setRequiredValue(e.target.value)}></input>
-        <input name = "lastname" placeholder = "last name" id="lastName" value = {requiredValue} onChange = {(e) => setRequiredValue(e.target.value)}></input>
+        <input type = "text"
+        name = "firstname" 
+        placeholder = "first name" 
+        id="firstName"
+        value = {inputFields.name}
+        onChange = {handleStateChange}
+        ></input>
+
+        <input type = "text" 
+        name = "lastname" 
+        placeholder = "last name" 
+        id="lastName"
+        value = {inputFields.surname}
+        onChange = {handleStateChange}
+        ></input>
+
   <h3>Please enter your email address:</h3>
-  <input name = "email" placeholder = "example@gmail.com" id = "email" value = {requiredValue} onChange = {(e) => setRequiredValue(e.target.value)} ></input>
-        <button type = "button" className = "nextButton" onClick = {nextButton} disabled = {requiredValue === ""}>Next</button>
+  <input type = "email" 
+  name = "email" 
+  placeholder = "example@gmail.com" 
+  id = "email"
+  value = {inputFields.email}
+  onChange = {handleStateChange}
+  ></input>
+        <button type = "button" className = "nextButton" onClick = {nextButton}>Next</button>
     </div>
 )}
-
 {currentPage === 2 && ( 
   <div className = "questionTwo">
     <h3>Will you be attending the wedding?</h3>
@@ -75,7 +113,6 @@ setCurrentPage(1);
     </p>
 </div>  
 )}
-
 {currentPage === 3 && (
 <div className = "sectionOne">
 <h3>Ceremony</h3>
@@ -93,7 +130,6 @@ setCurrentPage(1);
 <input type = "radio" id = "ceremonyAttendanceDeclined" name = "ceremonyAvailability" value = "Not Attending"></input>
     <label htmlFor = "ceremonyAttendanceDeclined">Not Attending</label>
 </p>
-
 {/**Reception*/}
 <h3>Reception</h3>
 <p>
@@ -101,6 +137,7 @@ setCurrentPage(1);
     <li>Saturday 12th October 2024, 13:30pm</li>
     <li>Cross Deep, Twickenham, London, TW1 4RB</li>
   </ul>
+  <input type = "radio" id="receptionAttendance" name = "receptionAvailibility" value = "Attending"></input>   
   <input type = "radio" id="receptionAttendance" name = "receptionAvailibility" value = "Attending" ></input>   
     <label htmlFor = "receptionAttendance">Attending</label>
 
@@ -111,9 +148,7 @@ setCurrentPage(1);
 <button type = "submit" id = "submitButtonSectionFour" onClick = {finished}>Finish</button>  
 </div>
 )}
-
 {/**Section Two */}
-
 {currentPage === 4 && (
   <div className = "sectionTwo">
 <h3>No worries, would you like to send a message to the newly wed couple? </h3>
@@ -129,7 +164,4 @@ setCurrentPage(1);
    </div>
     )
 };
-
-
 export default Pagination;
- 
