dd72558b46ea0e15c8b6224c94e8599d878e0fb1
Aaron Thomas <thomasattraction@yahoo.com>
2024-05-05 03:10:18 +0100

    New idea that utilizes my current iframe

--- src/components/pages/Playlist.js
+++ src/components/pages/Playlist.js
@@ -5,7 +5,14 @@ const Playlist = () => {
 
  return (
         <div className = "container">
-{/*I am going to use the Spotify API web app to create a playlist and alllow users to update that playlist on the website */}
+{/*I made an iframe that connects Spotify to my website. I have decided not to have users update the playlist on my website
+because I want the page to be focused on song requests. We can set a time limit before the start of the wedding
+for users to assign there song choice. The wedding is on October, we can have a title saying 'Do you have another song that you want to hear on our wedding? Submit a request here'
+Below the title we have a form and on that form a user can submit a response using a text type input attribute, 
+and that answer can be sent to the email of one of the married participants. If the couple are interested in the song,
+the owners can update the playlist. The playlist on my website will show previews of different songs,
+but on the day of the wedding, the full songs will be played either by the DJ or themselves (on my premium account without ads).
+If users want to submit another request then they can just type in another song of interest. */}
  <header>Playlist</header>
 <body>
     <Spotify />
--- src/components/pages/Spotify Fetch API.js
+++ src/components/pages/Spotify Fetch API.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Spotify = () => {  
 return (
     <div className="iframeContainer"> 
-        <iframe title= "myFrame" className = "embed-iframe" border-radius = {12} src="https://open.spotify.com/embed/playlist/3oAEtO3PlyJ7oGVw0EPYC5?utm_source=generator"width= {400} 
-        height= {400}> 
+        <iframe title= "myFrame" className = "embed-iframe" border-radius = {12} src="https://open.spotify.com/embed/playlist/3oAEtO3PlyJ7oGVw0EPYC5?utm_source=generator" width= {550} 
+        height= {500}> 
         </iframe> 
     </div>
 )
