6e9397a3c3fb290f7e8c96f001d0ab2790ed41cb
Aaron Thomas <thomasattraction@yahoo.com>
2024-05-05 01:39:22 +0100

    I am going to delete this API fetch request-I could just use an iframe instead

--- src/components/pages/Playlist.js
+++ src/components/pages/Playlist.js
@@ -1,7 +1,6 @@
 import React from 'react';
 const Playlist = () => {
-       
-  const clientID = `a8d53105487a4b5ea3cf59076eaf510a`;
+ const clientID = `a8d53105487a4b5ea3cf59076eaf510a`;
     const clientIdSecret = `f9249b73f9a54ecdb566215e27972a7e`;
 
     const authData = `${clientID} : ${clientIdSecret}`;
@@ -29,17 +28,53 @@ const Playlist = () => {
  })
  .catch (error => {
 console.log('Fetch error:',error)
- })
+ });
 
 
-  return (
+const userId = 1178425605;  
+const url = `https://api.spotify.com/v1/users/${userId}/playlists`;
+
+
+const data = {
+    "name": "Wedding Playlist",
+    "description": "Music playlist for my wedding",
+    "public": false
+};
+
+fetch(url, {
+   method: 'post',
+headers: { 'Authorization': 'Bearer 1POdFZRZbvb...qqillRxMr2z',
+'Content-Type': 'application/json'
+},
+body: JSON.stringify(data)
+})
+.then(response => {
+    if (!response.ok) {
+        throw new Error("The response was not okay")
+    }
+return response.JSON();
+})
+.then(data => {
+    console.log('Playlist created:', data);
+})
+.catch(error => {
+    console.error('There was a problem creating the playlist:', error);
+});
+ return (
         <div className = "container">
 {/*I am going to use the Spotify API web app to create a playlist and alllow users to update that playlist on the website */}
-<header>Playlist</header>
+ <header>Playlist</header>
 
         </div>
-    )
-}  
+    
+)};
+
+
+     
+ 
+
+ 
+
  
 
 
