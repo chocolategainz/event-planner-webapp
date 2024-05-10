39e1a9252492a8e985c6a2cd751a3437e9dcb97d
Aaron Thomas <thomasattraction@yahoo.com>
2024-05-10 14:42:25 +0100

    playing around with css elements

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -1,11 +1,21 @@
+import React from "react";
+import PictureTwo from "./PictureTwo.jpg";
+import Themes from "./Themes.css";
+
 const Homepage = () => {
     return ( 
+        
 <div className = "container">
+       
     <header>
     <h4>
     Home
     </h4>
     </header>
+
+    <main>
+<img src = {PictureTwo} alt = "Two people smiling" width = {1500}></img>
+    </main>
 </div>
 )};
 
