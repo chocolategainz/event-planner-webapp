905349659a0ea22bc98bbe917221e62ece9ad5d9
Aaron Thomas <thomasattraction@yahoo.com>
2024-05-11 16:33:19 +0100

    more changes to the About Us page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PictureTwo from './PictureTwo.jpg';
-import HappyCouple from './HappyCouple.jpg';
+import Names from './Names.jpg';
 import './Themes.css';
 
 const About = () => {
@@ -9,25 +9,22 @@ const About = () => {
     <header><h4><u>About Us</u></h4></header>
  
    <body> </body> 
-
- 
- 
-
+<h4 id = "aboutUsTitle">OUR STORY</h4>
 <div class="d-flex justify-content-evenly">
  <img src = {PictureTwo} className = "mainPicture" alt = "Two people smiling" height = {450}></img>
- <img src = {HappyCouple} className = "secondaryPicture" alt = "two people kissing" height = {450}></img>
-</div>
 
-<h4 id = "aboutUsTitle">HOW WE MET</h4>
     <p className = "description">
 {/*Placeholder */}
-Once upon a time, in the digital expanse of a dating website, our two hearts found solace in each other's words. 
+In the digital expanse of a dating website our two hearts found solace in each other's words. 
 With each exchanged message, our connection deepened, transcending screens and distances.
  As days turned into nights filled with endless conversations, we realized that we had stumbled upon a rare love, 
  one destined for eternity. With hesitant yet hopeful steps, we ventured beyond the virtual realm, forging bonds in the tangible world. 
  And as time wove its gentle tapestry, amidst laughter, tears, and shared dreams, we have decided to stand hand in hand, 
  saying "I do", so that our love story will eternally be etched in the stars.
+
+<p><img src = {Names} className = "cards" alt = "Names on cards" height = {300}></img></p> 
     </p>
+</div>
 
     
   
