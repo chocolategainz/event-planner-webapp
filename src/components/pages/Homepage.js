ee7092cd9ae072b104051b7514f3650a723ca0b3
Aaron Thomas <thomasattraction@yahoo.com>
2024-05-11 20:29:55 +0100

    slight design changes to the homepage

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -10,11 +10,9 @@ const Homepage = () => {
     <main className = 'boxContainer'>
 <p>IAN & JENNIFER</p>
 {/* <div className = 'borderLine'></div>*/}
-<p>24.10.2024</p>
+<p id = "weddingDate">24.10.2024</p>
 <button type="button" class="btn btn-success btn-lg">RSVP</button>
-
 </main>  
-
 </div>
 )};
 
