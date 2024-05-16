0403321b4cfd880b3765ab85c3bf9533b1ab322e
Aaron Thomas <thomasattraction@yahoo.com>
2024-05-16 14:11:12 +0100

    added a countdown timer to the homepage

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -10,10 +10,17 @@ const Homepage = () => {
     <main className = 'boxContainer'>
 <p>IAN & JENNIFER</p>
 {/* <div className = 'borderLine'></div>*/}
-<p id = "weddingDate">24.10.2024</p>
+<p id = "weddingDate">16.10.2024</p>
 <button type="button" id = "RSVP" class="btn btn-success btn-lg">RSVP</button>
 </main>  
+
+{/*Countdown API*/}
+<div id = "countdownTimer">
+    <img src="https://i.countdownmail.com/3aa851.gif?id=$2y$10$@/vvh.n*|URL:EMAIL|*" alt="countdownmail.com"/>
 </div>
+ </div>
+
+
 )};
 
 export default Homepage;
