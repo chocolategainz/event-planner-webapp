43c784f7c35eb025fe0c0943078985effc66a49d
Aaron Thomas <thomasattraction@yahoo.com>
2024-05-22 00:19:08 +0100

    added info to marker and it can open and close

--- src/components/pages/GoogleMapsApi.js
+++ src/components/pages/GoogleMapsApi.js
@@ -2,7 +2,7 @@ import {useState} from 'react';
 import {APIProvider, Map, Pin, InfoWindow, AdvancedMarker} from '@vis.gl/react-google-maps';
 
 const Maps = () => {
-
+const [open, setOpen] = useState(false);
 const apiKey = 'AIzaSyBUKYa1wpv8rwrPxbVn1RarGuC07sWHMmA';
 const location = {lat:51.44131201769951, lng:-0.3325682024467502};
 const options = {
@@ -20,9 +20,11 @@ return (
 <div>React Google Maps</div>   
 <div style = {{height: "100vh"}} >
 <Map minZoom = {10} center = {location} options = {options}>
-<AdvancedMarker position = {location}>
+<AdvancedMarker position = {location} onClick = {() =>setOpen (true)}>
     <Pin background = {'gold'} glyphColor={'red'} />
 </AdvancedMarker>
+{open && <InfoWindow position={location} onCloseClick={() => setOpen(false)}><p>Alexander Pope</p></InfoWindow>}
+
 </Map>
 </div>
 </APIProvider>
