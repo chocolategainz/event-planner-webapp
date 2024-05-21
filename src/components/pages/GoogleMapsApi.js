1f3383e65d20072d5a06b89fbd9a49d59abd10f8
Aaron Thomas <thomasattraction@yahoo.com>
2024-05-21 23:56:46 +0100

    set minimum zoom level to 10

--- src/components/pages/GoogleMapsApi.js
+++ src/components/pages/GoogleMapsApi.js
@@ -4,15 +4,14 @@ import {APIProvider, Map, Pin, InfoWindow, AdvancedMarker} from '@vis.gl/react-g
 const Maps = () => {
 
 const apiKey = 'AIzaSyBUKYa1wpv8rwrPxbVn1RarGuC07sWHMmA';
-const location = {lat:51.44131201769951,lng:-0.3325682024467502};
+const location = {lat:51.44131201769951, lng:-0.3325682024467502};
 const options = {
     zoomControl: true,
     fullscreenControl: true,
     mapTypeControl: true,
     streetViewControl: true,
     draggable: true,
-    zoom: 4,
-    maxZoom: 17,
+    zoom: 10,
     mapId: '97e73285245f7071'
   };
 
@@ -20,7 +19,7 @@ return (
 <APIProvider apiKey = {apiKey} >
 <div>React Google Maps</div>   
 <div style = {{height: "100vh"}} >
-<Map zoom = {9} center = {location} options = {options}>
+<Map minZoom = {10} center = {location} options = {options}>
 <AdvancedMarker position = {location}>
     <Pin background = {'gold'} glyphColor={'red'} />
 </AdvancedMarker>
