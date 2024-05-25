c41464c9f793163bd6b3561d499b98c42e2602d4
Aaron Thomas <thomasattraction@yahoo.com>
2024-05-25 19:05:35 +0100

    moved files back to src

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Navbar from './components/pages/Navbar';
 import Homepage from './components/pages/Homepage';
 import About from './components/pages/About';
 import Contacts from './components/pages/Contacts';
-
+import Form from './components/pages/Form';
+import Playlist from './components/pages/Playlist';
 
 /*Functional Component*/
 function App() {
-  return (
-    <div className="Nav">
-      
+
+  return (  
+<div className="Nav">  
       <Router>
       <header className="">
       <Navbar /> 
@@ -18,10 +19,12 @@ function App() {
         <Route exact path = "/" element = {<Homepage />} />
         <Route exact path = "/About" element = {<About />} />
         <Route exact path = "/Contacts" element = {<Contacts />} />
+        <Route exact path = "/Form" element = {<Form />} />
+        <Route exact path = "/Playlist" element = {<Playlist />} />
        </Routes>
       </Router>
-
     </div>
+   
   );
 }
 
